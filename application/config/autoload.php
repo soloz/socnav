7ea2b771a39620ee8467b0479125af575f4a7dca
Solomon Adebayo <solomoniyi@gmail.com>
2013-04-06 03:30:26 -0700

    adding autoload libraries (database, session, form_validation) helper files (url, html, forms) and model files (user.php) to config/autoload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,8 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array();
-
+$autoload['libraries'] = array('database', 'session', 'form_validation');
 
 /*
 | -------------------------------------------------------------------
@@ -64,7 +63,7 @@ $autoload['libraries'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array();
+$autoload['helper'] = array('url', 'form', 'html');
 
 
 /*
@@ -109,7 +108,7 @@ $autoload['language'] = array();
 |
 */
 
-$autoload['model'] = array();
+$autoload['model'] = array('user');
 
 
 /* End of file autoload.php */
