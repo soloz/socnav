28305e348c0239ec2b6708f11f2e22d9d2232626
nm67 <nmoumoulides@gmail.com>
2013-04-11 16:23:12 +0100

    added testjson.php for starting to test json calls

---
+++ application/views/testjson.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+  </body>
+</html>
+<?php
+$arr = array('a' => 33, 'b' => 22, 'c' => 55, 'd' => 44, 'e' => 66);
+
+echo json_encode($arr);
+?>
